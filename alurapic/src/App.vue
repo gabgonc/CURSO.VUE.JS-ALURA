1ca508a6c50f43a30c102cda688bf4c02cff94b4
gabgonc <gabrielpgonc@outlook.com>
2021-10-02 00:10:53 -0300

    Módulo 3 - Integrando API de terceiros

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -2,40 +2,43 @@
   <div>
     <h1>{{ titulo }}</h1>
 
-
     <ul>
-
       <li v-for="foto of fotos">
         <img :src="foto.url" :alt="foto.titulo">
       </li>
     </ul>
 
   </div>
-    
 </template>
 
 <script>
 export default {
-  
-  data(){
+
+  data() {
 
     return {
-      titulo: 'Alurapic',
-      fotos: [
-        {
-        url: 'https://super.abril.com.br/wp-content/uploads/2018/05/filhotes-de-cachorro-alcanc3a7am-o-c3a1pice-de-fofura-com-8-semanas1.png',
-        titulo: 'cachorro'
-        },
-        {
-        url: 'https://super.abril.com.br/wp-content/uploads/2018/05/filhotes-de-cachorro-alcanc3a7am-o-c3a1pice-de-fofura-com-8-semanas1.png',
-        titulo: 'cachorrao'
-        }
-      ]
+
+      titulo: 'Alurapic', 
+      fotos: []
     }
+  },
+
+  created(){
+
+    this.$http.get('http://localhost:3000/v1/fotos')
+    //promise
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos)
+      // promise.then(res => {
+
+      //   res.json().then(fotos => this.fotos = fotos)
+      // })
+
   }
+
 }
+
 </script>
 
 <style>
-
 </style>
